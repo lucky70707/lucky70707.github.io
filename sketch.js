b1e8c592a494319b5f74cb587d079efcf490f6e3
unknown <zoetepopcorn@live.nl>
2021-06-18 17:35:56 +0200

    moved website stuff

--- sketch.js
+++ sketch.js
@@ -1,156 +1,37 @@
+var cols, rows;
+var w =40;
+var grid= [];
 
-var paddle;
-var bgA; //short for background animation
-var ball;
-var bricks = [];
-var playingGame = false;
-var youWin =false;
-var aPressed = false;
-var dPressed = false;
-var winText;
-var initText;
-var level;
+function  setup(){
+  createCanvas(400,400);
+  cols =floor(width/w);
+  rows = floor(height/w)
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  paddle = new Paddle();
-  bgA = new BackgroundAnimation();
-  ball = new Ball();
-  this.level = 1;
-  for(let i = 0; i <20; i++){
-    bricks.push(new Brick());
+  for(var j =0; j<rows;j++){
+    for(var i=0;i<cols;i++){
+      var cell = new Cell(i,j);
+      grid.push(cell);
+    }
   }
-  createInitialText();
-  createText();
 }
 
-function draw() {
-  background(bgA.rValue, bgA.gValue, bgA.bValue);
-
-  paddle.display();
-  ball.display();
-
-  if(playingGame){
-    paddle.update();
-    paddle.checkEdges();
-    ball.update();
-    ball.checkEdges();
-    initText.style('display','none');
-  }else{
-    initText.style('display','block');
+function draw(){
+  background(51)
+  for(var i =0; i<grid.length;i++){
+    grid[i].show();
   }
-  if(this.level === 2){
+}
+
+
+function Cell(i,j){
+  this.i = i;
+  this.j =j;
+
+  this.show = function(){
+    var x=this.i*w;
+    var y= this.j*w;
+    stroke(255);
     noFill();
+    rect(x,y,w,w);
   }
-
-
-if(youWin){
-  winText.style('display','block');
-  ball.direction.x = 1;
-
-}else {
-  winText.style('display','none');
-}
-
-
-  if(ball.meets(paddle) && ball.direction.y > 0 ){
-      if(paddle.dir === 1){//paddle goes to the right
-        if(ball.direction.x> 0){
-            ball.direction.x =1.5;
-        }else {
-          ball.direction.x +=0.5;
-        }
-
-      }else if (paddle.dir===-1) {//paddle goes to the left
-        if(ball.direction.x < 0){
-          ball.direction.x =-1.5;
-        }else{
-          ball.direction.x -=0.5;
-        }
-
-      }
-
-
-    ball.direction.y *= -1;
-  }
-
-  for(var i = 0; i <bricks.length;i++){
-    bricks[i].display();
-    if(ball.hits(bricks[i])){
-      if(bricks[i].r>40){
-        bricks[i].r= bricks[i].r / 2 ;
-
-        bgA.gChange(5);
-      }else{
-        bricks.splice(i, 1);
-          bgA.gChange(-5);
-      }
-      ball.direction.y *= -1;
-    }
-
-  }
-  if(ball.pos.y > height){//aka if you lose
-    playingGame = false;
-    ball.direction.x =1;
-    ball.pos = createVector(width /2 , height /2);
-    paddle.pos = createVector(width /2 , height - 40);
-  }
-  if(bricks.length ===0){
-    youWin = true;
-    this.level =2;
-    bgA.bValue = 250;
-    playingGame = false;
-  }
-  //bgA.rUpdate();
-}
-function keyReleased(){
-  if (key === 'a' || key === 'A') {
-    this.aPressed =false;//maybe not this. ?
-  }
-
-  else if (key === 'd' || key === 'D') {
-    this.dPressed =false;
-}
-
-  if(aPressed === false && dPressed == false){
-    paddle.dir = 0;
-  }
-}
-function keyPressed() {
-  if (key === 'a' || key === 'A') {
-    paddle.dir=-1;
-    bgA.rChange(50);
-    this.aPressed =true;//maybe not this. ?
-    bgA.rUp = true;
-  }
-
-  else if (key === 'd' || key === 'D') {
-    paddle.dir=1;
-    this.dPressed =true;
-    bgA.rChange(-50);
-    //  if (backgroundValue < 256) {
-    //  backgroundValue+=15;
-    //  bgA.cUp = true;
-  }else if(key === 's'|| key==='S'){
-    playingGame = true;
-    youWin = false;
-    if(bricks.length === 0){
-      for(let i = 0; i<20; i++){
-        bricks.push(new Brick());
-      }
-      ball.pos = createVector(width /2 , height /2);
-      paddle.pos = createVector(width /2 , height - 40);
-    }
-  }
-
-}
-function createInitialText (){
-  initText = createP("Press 's' to start the game, move the paddle with 'a' and 'd'");
-  initText.position(width / 4 -75, 100);
-}
-
-function createText(){
-  winText = createP('You Win!')
-
-  winText.position(width / 2 -50, 80);
 }
